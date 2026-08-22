ce49a06356274fd30bb978d49bfd539f3548dfad
denis-mahei <denmahei@gmail.com>
2026-08-22 19:48:46 +0300

    feat(projects):added animation for projects list

--- components/projects/data.ts
+++ components/projects/data.ts
@@ -4,6 +4,7 @@ import {
   RiReactjsLine,
   RiHtml5Fill,
   RiNodejsLine,
+  RiReactjsFill,
 } from '@remixicon/react';
 import { SiNestjs } from '@icons-pack/react-simple-icons';
 
@@ -20,7 +21,14 @@ export type ProjectTag =
   | 'mongodb'
   | 'zustand'
   | 'axios'
-  | 'nodejs';
+  | 'nodejs'
+  | 'redux'
+  | 'jwt-auth';
+
+export type codeUrl = {
+  frontend: string;
+  backend?: string;
+};
 
 export type Project = {
   id: string;
@@ -29,8 +37,9 @@ export type Project = {
   description: string;
   tags: ProjectTag[];
   liveUrl: string;
-  codeUrl: string;
+  codeUrl: codeUrl;
   image: string;
+  icon: RemixiconComponentType;
 };
 
 export const projects: Project[] = [
@@ -38,21 +47,60 @@ export const projects: Project[] = [
     id: 'note-hub',
     name: 'note-hub',
     displayName: '_note-hub',
-    description: 'Fullstack development project',
+    description:
+      'Notes management app with tag filtering, search, and cookie-based auth with silent JWT refresh.',
     tags: ['next', 'typescript', 'shadcn/ui', 'tailwindcss', 'axios'],
     liveUrl: 'https://e-pharmacy.vercel.app',
-    codeUrl: 'https://github.com/denys-mahei/e-pharmacy',
+    codeUrl: {
+      frontend: 'https://note-hub-app-jet.vercel.app/',
+    },
     image: '/notehub.webp',
+    icon: RiNextjsFill,
   },
   {
     id: 'read-journey',
     name: 'read-journey',
     displayName: '_read-journey',
-    description: 'Fullstack development engineering',
+    description:
+      'A modern web application for managing your personal reading library, tracking reading progress, and discovering new books.',
     tags: ['next', 'typescript', 'zustand', 'axios', 'tailwindcss'],
-    liveUrl: 'https://e-pharmacy.vercel.app',
-    codeUrl: 'https://github.com/denys-mahei/e-pharmacy',
+    liveUrl: 'https://read-journey-h46z.vercel.app',
+    codeUrl: {
+      frontend: 'https://github.com/denis-mahei/read-journey',
+    },
     image: '/readjourney.webp',
+    icon: RiNextjsFill,
+  },
+  {
+    id: 'contacts-manager',
+    name: 'contacts-manager',
+    displayName: '_contacts-manager',
+    description:
+      'A full-stack contact management application with authentication, built with React (frontend) and a Node.js/Express backend API.',
+    tags: ['react', 'redux', 'mui', 'axios', 'jwt-auth'],
+    liveUrl: 'https://contact-book-manager-dm.vercel.app',
+    codeUrl: {
+      frontend: 'https://github.com/denis-mahei/contact-manager-app',
+      backend: 'https://github.com/denis-mahei/nodejs-contacts-api',
+    },
+    image: '/contacts.webp',
+    icon: RiReactjsLine,
+  },
+  {
+    id: 'e-pharmacy',
+    name: 'e-pharmacy',
+    displayName: '_e-pharmacy',
+    description:
+      'Full-stack pharmacy admin panel with product, supplier, order and customer management.',
+    tags: ['next', 'nestjs', 'mui', 'prisma'],
+    liveUrl: 'https://admin-dashboard-frontend-blush.vercel.app',
+    codeUrl: {
+      frontend:
+        'https://github.com/denis-mahei/admin-dashboard-frontend',
+      backend: 'https://github.com/denis-mahei/admin-dashboard-api',
+    },
+    image: '/e-pharmacy.webp',
+    icon: RiNextjsFill,
   },
 ];
 
--- components/projects/projects-list.tsx
+++ components/projects/projects-list.tsx
@@ -3,20 +3,73 @@
 import { useSelectedTags } from '@/lib/hooks/useSelectedTags';
 import { filteredProjects } from '@/lib/utils/filtered-projects';
 import { projects } from '@/components/projects/data';
+import { AnimatePresence, motion } from 'motion/react';
+import Image from 'next/image';
 
 function ProjectsList() {
   const selectedTags = useSelectedTags();
   const projectsList = filteredProjects(projects, selectedTags);
-  console.log(projectsList);
   return (
     <div className="p-6 lg:p-16">
-      <ul className="grid grid-cols-3 gap-2.5">
-        {projectsList.length > 0 &&
-          projectsList.map((project) => (
-            <li key={project.id}>{project.name}</li>
+      <ul className="grid grid-cols-1 lg:grid-cols-3 gap-2.5">
+        <AnimatePresence>
+          {projectsList.map((project, idx) => (
+            <motion.li
+              layout
+              initial={{ transform: 'translateY(-100px)' }}
+              animate={{ transform: 'translateY(0px)' }}
+              transition={{ type: 'spring', delay: idx * 0.1 }}
+              key={project.id}
+              className="perspective-distant"
+            >
+              <h3 className="font-bold text-indigo-500 mb-4">
+                Project {idx + 1}{' '}
+                <span className="font-normal text-foreground">
+                  &#x2f;&#x2f; {project.displayName}
+                </span>
+              </h3>
+              <article className="border border-slate-800 rounded-2xl flex flex-col overflow-hidden transition-transform duration-300 ease-out hover:rotate-y-8 hover:rotate-x-6 hover:shadow-2xl hover:scale-105">
+                <div className="relative h-36">
+                  <Image
+                    src={project.image}
+                    alt={`Screenshot of ${project.name}`}
+                    fill
+                  />
+                  <project.icon
+                    className="absolute p-1 bg-indigo-300 rounded-md right-2 top-2"
+                    size={28}
+                    color="slate-900"
+                  />
+                </div>
+                <div className="bg-slate-950 p-4 flex flex-col gap-5">
+                  <p className="line-clamp-2">
+                    {project.description}
+                  </p>
+                  <div className="flex gap-2">
+                    <a
+                      href={project.codeUrl.frontend}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="buttons"
+                    >
+                      view-code
+                    </a>
+                    <a
+                      href={project.liveUrl}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="buttons"
+                    >
+                      view-demo
+                    </a>
+                  </div>
+                </div>
+              </article>
+            </motion.li>
           ))}
-        {projectsList.length === 0 && <p>No projects found.</p>}
+        </AnimatePresence>
       </ul>
+      {projectsList.length === 0 && <p>No projects found.</p>}
     </div>
   );
 }
