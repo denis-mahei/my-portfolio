9fab53c5bb66eae4f42faee31e50bcbca7e8c1ac
denis-mahei <denmahei@gmail.com>
2026-08-26 16:53:54 +0300

    feat(home): add screw icon to card corners

--- components/home/hero-interactive.tsx
+++ components/home/hero-interactive.tsx
@@ -1,6 +1,7 @@
 'use client';
 import GlowBackground from '@/components/home/glow-background';
 import { motion } from 'motion/react';
+import Screw from '@/components/ui/screw';
 
 function HeroInteractive() {
   return (
@@ -11,7 +12,12 @@ function HeroInteractive() {
         className="relative w-126.75 h-117.25 border border-stroke rounded-lg backdrop-blur-3xl shadow-[inset_0_2px_0_0_rgba(255,255,255,0.3)] bg-(image:--card-gradient)"
       >
         <GlowBackground />
-        Card
+
+        <Screw position="absolute top-5 left-5" />
+
+        <Screw position="absolute top-5 right-5" />
+        <Screw position="absolute bottom-5 left-5" />
+        <Screw position="absolute bottom-5 right-5" />
       </motion.div>
     </div>
   );
---
+++ components/ui/screw.tsx
@@ -0,0 +1,26 @@
+type ScrewProps = {
+  position: string;
+};
+
+function Screw({ position }: ScrewProps) {
+  return (
+    <div
+      className={`w-5 h-5 flex items-center justify-center rounded-full bg-[radial-gradient(circle_at_center,#217D7A_15%,#114B4A_100%)] shadow-[2px_2px_4px_0_#092D32] inset-shadow-[0_1px_2px_0_#16645C] ${position}`}
+    >
+      <svg
+        width="7"
+        height="6"
+        viewBox="0 0 7 6"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="text-teal-900"
+      >
+        <path
+          d="M0.318848 4.9744L5.85742 0.38501M0.318848 0.38501L5.85742 4.9744"
+          stroke="currentColor"
+        />
+      </svg>
+    </div>
+  );
+}
+export default Screw;
